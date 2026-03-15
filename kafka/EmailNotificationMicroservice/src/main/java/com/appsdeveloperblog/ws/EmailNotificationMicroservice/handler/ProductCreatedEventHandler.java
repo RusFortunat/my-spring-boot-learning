7bfb7f654cbce1fa6944348dc69528c467a0bf48
ruslan <ruslan.mukhamadiarov@gmail.com>
2026-03-15 14:46:52 +0100

    feat: [main] changes: -- DLT is properly configured

--- kafka/EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/handler/ProductCreatedEventHandler.java
+++ kafka/EmailNotificationMicroservice/src/main/java/com/appsdeveloperblog/ws/EmailNotificationMicroservice/handler/ProductCreatedEventHandler.java
@@ -1,21 +1,29 @@
 package com.appsdeveloperblog.ws.EmailNotificationMicroservice.handler;
 
+import com.appsdeveloperblog.ws.core.ProductCreatedEvent;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
-import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
-@KafkaListener(topics="product-created-events-topic")
 public class ProductCreatedEventHandler {
 
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    @KafkaHandler
-    public void handle(ProductCreatedEvent productCreatedEvent){
+    @KafkaListener(topics="product-created-events-topic", groupId = "product-created-events")
+    public void handle(@Payload ProductCreatedEvent event){
 
-        LOGGER.info("Received a new event: " +  productCreatedEvent.getTitle());
+        throw new RuntimeException("Test DLT routing");
+
+//        if (event == null) {
+//            // deserialization failed, check headers for the error
+//            LOGGER.error("Failed to deserialize message");
+//            return;
+//        }
+//
+//        LOGGER.info("Received a new event: " +  event.getTitle());
 
     }
 
